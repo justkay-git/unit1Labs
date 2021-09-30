2d016e0d9aeec6e39dca18252a4cb6d37fafe112
justkay-git <60712212+justkay-git@users.noreply.github.com>
2021-09-29 22:55:47 -0400

    initail

---
+++ leaverson-part-two/js/script.js
@@ -0,0 +1,18 @@
+console.log("Hello world!");
+document.body.style.backgroundColor = "black";
+/* the white background was too much to handle during 10pm at night*/
+/*
+Step 1: prompt user for pet name
+    - store user input in variable (petName)
+Step 2: Set Pet energy and happiness levels to zero
+    - create variables (energylvl), (happylvl)
+Step 3: declare feed, pet, walk, options
+    ~ feed = 2+ energy increase
+    ~ pet = 1+ happiness increase
+    ~ walk = 2+ happiness increase & 1- energy
+        - if user chooses walk & energy lvl = 0, do NOT perform actions.
+       instead, prompt alert message: "Not enough energy to enjoy a walk."
+Step 4: create loop that repeats 6 times the following
+    - prompt user for an action input
+Step 5: after 6 successful actions log petName, happiness, and energy lvls.
+*/
